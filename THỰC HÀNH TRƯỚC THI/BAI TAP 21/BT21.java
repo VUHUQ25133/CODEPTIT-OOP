3d11d5f5976e1d71dafb79b00dc7a6beee8a3aa8
VUHUQ25133 <146147897+VUHUQ25133@users.noreply.github.com>
2024-12-11 00:15:32 +0700

    Update BT21.java

--- THỰC HÀNH TRƯỚC THI/BAI TAP 21/BT21.java
+++ THỰC HÀNH TRƯỚC THI/BAI TAP 21/BT21.java
@@ -55,3 +55,57 @@ public class Main {
             System.out.println(x);
     }
 }
+//Cách 2
+// class sinhVien implements Comparable<sinhVien> {
+//     private String hoTen;
+//     private int soBai, submit;
+
+//     public sinhVien(String hoTen, int soBai, int submit) {
+//         this.hoTen = hoTen;
+//         this.soBai = soBai;
+//         this.submit = submit;
+//     }
+
+//     public String getHoTen() {
+//         return hoTen;
+//     }
+
+//     public int getSoBai() {
+//         return soBai;
+//     }
+
+//     public int getSubmit() {
+//         return submit;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return this.hoTen.trim() + " " + this.soBai + " " + this.submit;
+//     }
+//     @Override
+//     public int compareTo(sinhVien o) {
+//         if (this.soBai != o.soBai) {
+//             return o.soBai - this.soBai;
+//         }
+//         if (this.submit != o.submit) {
+//             return this.submit - o.submit;
+//         }
+//         return this.hoTen.compareTo(o.hoTen);
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) throws IOException {
+//         Scanner sc = new Scanner(new File("LUYENTAP.in"));
+//         int n = sc.nextInt();
+//         sinhVien[] sv = new sinhVien[n];
+//         for (int i = 0; i < n; i++) {
+//             sc.nextLine(); 
+//             sv[i] = new sinhVien(sc.nextLine(), sc.nextInt(), sc.nextInt());
+//         }
+//         Arrays.sort(sv);
+//         for (sinhVien x : sv) {
+//             System.out.println(x);
+//         }
+//     }
+// }
