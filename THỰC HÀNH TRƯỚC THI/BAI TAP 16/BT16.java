049d5f62f2f8132a8f29bdbfbe3e5ef5d83a07b5
VUHUQ25133 <146147897+VUHUQ25133@users.noreply.github.com>
2024-12-10 23:53:10 +0700

    Update BT16.java

--- THỰC HÀNH TRƯỚC THI/BAI TAP 16/BT16.java
+++ THỰC HÀNH TRƯỚC THI/BAI TAP 16/BT16.java
@@ -11,16 +11,29 @@ public class Main {
         ArrayList<String> list = (ArrayList<String>) ois.readObject();
         ois.close();
 
+        //Cách 1
         for (String s : list) {
-            StringBuilder bin = new StringBuilder();
-            for (char c : s.toCharArray()) {
-                if (c == '0' || c == '1') {
-                    bin.append(c);
-                }
+            // Loại bỏ tất cả các ký tự không phải 0 hoặc 1
+            String bin = s.replaceAll("[^01]", "");
+            if (!bin.isEmpty()) {
+                long result = Long.parseLong(bin, 2);
+                System.out.println(bin + " " + result);
             }
-
-            long result = Long.parseLong(bin.toString(), 2);
-            System.out.println(bin.toString() + " " + result);
         }
+        //Cách 2
+        // for (String s : list) {
+        //     StringBuilder bin = new StringBuilder();
+        //     for (char c : s.toCharArray()) {
+        //         if (c == '0' || c == '1') {
+        //             bin.append(c);
+        //         }
+        //     }
+
+        //     long result = Long.parseLong(bin.toString(), 2);
+        //     System.out.println(bin.toString() + " " + result);
+        // }
+
+
+        
     }
 }
